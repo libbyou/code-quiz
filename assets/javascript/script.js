5cb81e48d68590e6e56963bf64f332997011fdbf
libbyou <lcyou95@gmail.com>
2023-10-04 17:07:49 -0500

    edits made to index.html, script.js, and style.css; also added a high scores page

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,29 +1,172 @@
 var timeEl = document.querySelector(".time");
 var startButton = document.querySelector(".start-button");
+// var askedQuestion = document.getElementById("asked-question")
+// var answerChoices = document.getElementById("answer-choices")
 
-var questionOne = ["Which of these is not a common data type in JavaScript?", "Strings", "Booleans", "Numbers", "Timers"];
-var questionTwo = ["What does 'this' refer to when used alone?", "The nearest object written in the code", "The JavaScript file", "The global window object", "What the user's mouse is hovering over"];
-var questionThree = ["HTML and CSS are capable of creating the same dynamic functunality that JavaScript can."];
-var questionFour = ["What keyword do you use to declare a new variable in JavaScript?"];
-var questionFive = ["Which of these operators means strictly equals?", "=", "!==", "==", "==="];
+var questionsArray = [
+    {
+    question: "Which of these is not a common data type in JavaScript?",
+    answers: {
+        a: "Strings",
+        b: "Booleans",
+        c: "Numbers",
+        d: "Timers",
+    },
+    correctAnswer: "Timers",
+    },
+    {
+    question: "What does 'this' refer to when used alone?",
+    answers: {
+        a: "The nearest object written in the code",
+        b: "The JavaScript file", 
+        c: "The global window object",
+        d:"What the user's mouse is hovering over",
+    },
+    correctAnswer: "The global window object",
+    },
+    {
+    question: "HTML and CSS are capable of creating the same dynamic functunality that JavaScript can.",
+    answers: {
+        a: "true",
+        b: "false",
+    },
+    correctAnswer: "false",
+    },
+    {
+    question: "What's the keyword you use to declare a new variable in JavaScript?",
+    answers: {
+        a: "var",
+        b: "for",
+        c: "function",
+        d: "console.log",
+     },
+    correctAnswer: "var",
+    },
+    {
+    question: "Which of these operators means strictly equals?",
+    answers: {
+        a: "=",
+        b: "!==",
+        c: "==",
+        d: "===",
+    },
+    correctAnswer: "===",
+    },
+    ];
+
 
 function startGame() {
-    secondsLeft = 60;
     startTime();
+    askQuestions();
 };
 
+var secondsLeft = 60;
 
 function startTime() {
-    var timerInterval = setInterval (function () {
+    var timerInterval = setInterval (function() {
         secondsLeft--;
-        timeEl.textcontent = "Time: " + secondsLeft;
+        timeEl.textContent = "Time: " + secondsLeft;
        
-        if(secondsLeft === 0) {
-            clearInterval(timerInterval);
-          }
-      
-        }, 1000);
+    if(secondsLeft === 0) {
+        clearInterval(timerInterval);
+        }
+    }, 1000);
+};
+ var i = 0
+
+function askQuestions () {
+    // currentQuestion = 0;
+        console.log (questionsArray[i].question);
+        var question = questionsArray[i].question;
+        var stringIndexQs = i.toString();
+        var questionDiv = ("question-" + stringIndexQs);
+        console.log(questionDiv)
+        var parentDiv =  document.getElementById(questionDiv)
+        var questionContainer = document.querySelector(".asked-question")
+        console.log(questionContainer)
+        questionContainer.textContent = question
+        
+        console.log (questionsArray[i].answers["a"]);
+        var optionA = questionsArray[i].answers["a"];
+        console.log ("this is the console log for option A", optionA)
+        console.log (questionsArray[i].answers["b"]);
+        var optionB = questionsArray[i].answers["b"];
+
+        console.log (questionsArray[i].answers["c"]);
+        var optionC = questionsArray[i].answers["c"];
+
+        console.log (questionsArray[i].answers["d"]);
+        var optionD = questionsArray[i].answers["d"];
+
+        // var answerContainer = parentDiv.getElementsByClassName("answer-choices")
+        // console.log(answerContainer)
+        
+        var firstChoice =document.getElementById("buttonOne")
+        console.log("Choice One", firstChoice)
+        firstChoice.textContent=optionA
+        firstChoice.addEventListener("click", nextQuestion)
+        // function (){
+        //     i++;
+        //     if (i < questionsArray.length) {askQuestions();}
+            
+        // })
+        var secondChoice = document.getElementById ("buttonTwo")
+        console.log("Choice Two", secondChoice)
+        secondChoice.textContent=optionB
+        secondChoice.addEventListener("click", nextQuestion);
+        // function (){
+        //     i++;
+        //     if (i < questionsArray.length) {askQuestions();}
+            
+        // })
+
+        var thirdChoice = document.getElementById("buttonThree")
+        console.log("Choice Three", thirdChoice)
+        thirdChoice.textContent=optionC
+        thirdChoice.addEventListener("click", nextQuestion)
+        // function (){
+        //     i++;
+        //     if (i < questionsArray.length) {askQuestions();}
+            
+        // })
+
+        var fourthChoice = document.getElementById("buttonFour")
+        console.log("Choice Four", fourthChoice)
+        fourthChoice.textContent=optionD
+        fourthChoice.addEventListener("click", nextQuestion);
+        // function (){
+        //     i++;
+        //     if (i < questionsArray.length) {askQuestions();}
+            
+        // })
+
+    // if (===correctAnswer) {
+
+    // } else {
+
+    // }
+
+    
 };
 
 
+function nextQuestion (event) {
+    var clickedButton = event.target;
+    if (clickedButton.textContent!==questionsArray[i].correctAnswer) {
+        secondsLeft=secondsLeft-10;
+    }
+    i++;
+    if (i < questionsArray.length) {askQuestions();}
+    else {gameOver()};
+}
+
+function gameOver() {
+    var gameOverPage = document.getElementById("game-over-page")
+    gameOverPage.classList.remove("hidden")
+    var quizPage = document.getElementById("game-section")
+    quizPage.classList.add("hidden");
+}
+
 startButton.addEventListener("click", startGame);
+
+
